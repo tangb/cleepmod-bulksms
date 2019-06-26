876afbb87eed78dbdb5c472bb2b71f8ec9a6c8d5
Tangb <tanguy.bonneau@gmail.com>
2019-06-26 19:53:10 +0200

    Remove useless member

--- backend/bulksms.py
+++ backend/bulksms.py
@@ -22,7 +22,6 @@ class Bulksms(RaspIotRenderer):
     MODULE_PRICE = 0
     MODULE_DEPS = []
     MODULE_DESCRIPTION = u'Sends you SMS alerts using BulkSms gateway.'
-    MODULE_LOCKED = False
     MODULE_TAGS = [u'sms', u'alert']
     MODULE_COUNTRY = None
     MODULE_URLINFO = u'https://github.com/tangb/Raspiot/wiki/Bulksms'
